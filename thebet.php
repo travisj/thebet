da7340433ae05d63131dc85eb14c6221e724ba2c
travis <travisj@gmail.com>
2010-04-04 10:58:04 -0400

    updating script

--- thebet.php
+++ thebet.php
@@ -1,11 +1,13 @@
 <?php
-$base = dirname(__FILE__);
+date_default_timezone_set('America/New_York');
+$base = $argv[1]; //dirname(__FILE__);
 
-$teams = json_decode(file_get_contents($base . '/teams.json'), true);
-$picks = json_decode(file_get_contents($base . '/picks.json'), true);
-$players = json_decode(file_get_contents($base . '/players.json'), true);
+$teams = json_decode(file_get_contents($base . '/_json/teams.json'), true);
+$picks = json_decode(file_get_contents($base . '/_json/picks.json'), true);
+$players = json_decode(file_get_contents($base . '/_json/players.json'), true);
+$history = json_decode(file_get_contents($base . '/_json/history.json'), true);
 
-$url = 'http://espn.go.com/mlb/standings/_/year/2009/seasontype/2';
+$url = 'http://espn.go.com/mlb/standings/_/year/2010/seasontype/2';
 $handle = fopen($url, "rb");
 
 $contents = '';
@@ -24,11 +26,31 @@ foreach($matches[0] as $i=>$match) {
 }
 
 $scores = array();
+$specifics = array();
 foreach ($picks as $team => $pick) {
 	if (!isset($scores[$pick['owner']])) {
 		$scores[$pick['owner']] = 0;
 	}
 	$scores[$pick['owner']] += $results[$team][$pick['choice']];
+	$specifics[$team] = $results[$team][$pick['choice']];
 }
 
-var_dump($scores);exit;
+$file = $base . '/_posts/' . date('Y-m-d') . '-Results.markdown';
+
+$page = "---\n";
+$page .= "layout: post\n";
+foreach ($scores as $owner => $score) {
+	$history[date('Y-m-d')][$owner] = $score;
+	$page .= $owner . ': ' . $score . "\n";
+}
+foreach ($specifics as $t => $s) {
+	$page .= "$t: $s\n";
+}
+$page .= "---\n";
+
+file_put_contents($base . '/_json/history.json', json_encode($history));
+file_put_contents($file, $page);
+system('cd ' . $base . ';' .
+		'git add ' . $file .';' .
+		'git commit -m "adding todays results";' .
+		'git push origin gh-pages');
